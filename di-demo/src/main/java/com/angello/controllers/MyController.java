c191c472a3634419ab13f614a74de7c4f7eeeaff
Angello Manrique <angellom90@gmail.com>
2017-11-02 12:26:35 +0100

    adding controller example

---
+++ di-demo/src/main/java/com/angello/controllers/MyController.java
@@ -0,0 +1,16 @@
+package com.angello.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jt on 5/23/17.
+ */
+@Controller
+public class MyController {
+
+    public String hello(){
+        System.out.println("Hello!!! ");
+
+        return "foo";
+    }
+}
