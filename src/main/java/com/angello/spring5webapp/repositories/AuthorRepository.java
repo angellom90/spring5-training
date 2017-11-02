0e1e64bbb3917b8a73067145ec5fc18057e4c679
Angello Manrique <angellom90@gmail.com>
2017-11-02 09:44:04 +0100

    spring-data-jpa

---
+++ src/main/java/com/angello/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,10 @@
+package com.angello.spring5webapp.repositories;
+
+import com.angello.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jt on 5/16/17.
+ */
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
---
+++ src/main/java/com/angello/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,10 @@
+package com.angello.spring5webapp.repositories;
+
+import com.angello.spring5webapp.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jt on 5/16/17.
+ */
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
