c0497acad6f08eea07cc0db1ead35dd86b3fc0c8
Angello Manrique <angellom90@gmail.com>
2017-11-02 12:06:00 +0100

    Spring 5 Dependency Injection Demo initial commit

---
+++ di-demo/src/main/java/com/angello/DiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.angello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiDemoApplication.class, args);
+	}
+}
