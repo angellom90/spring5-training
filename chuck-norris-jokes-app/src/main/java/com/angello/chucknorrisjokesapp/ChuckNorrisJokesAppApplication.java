236c95c0235b4bdf19e4728ebf65a4e84eacd40d
Angello Manrique <angellom90@gmail.com>
2017-11-03 13:03:55 +0100

    changing to xml configuration file

--- chuck-norris-jokes-app/src/main/java/com/angello/chucknorrisjokesapp/ChuckNorrisJokesAppApplication.java
+++ chuck-norris-jokes-app/src/main/java/com/angello/chucknorrisjokesapp/ChuckNorrisJokesAppApplication.java
@@ -1,9 +1,12 @@
 package com.angello.chucknorrisjokesapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class ChuckNorrisJokesAppApplication {
 
 	public static void main(String[] args) {
