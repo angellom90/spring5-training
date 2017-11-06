4491b9d6a2c9da36583fb4b2529395b461454f1c
Angello Manrique <angellom90@gmail.com>
2017-11-06 19:15:29 +0100

    develoment tools sample

---
+++ recipe-project/src/main/java/com/angello/recipeproject/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.angello.recipeproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jt on 6/1/17.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        System.out.println("test");
+        return "index";
+    }
+}
