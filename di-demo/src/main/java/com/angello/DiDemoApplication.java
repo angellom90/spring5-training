3893af92fd92b279d162bc0a39fadb1cb7497432
Angello Manrique <angellom90@gmail.com>
2017-11-06 16:36:47 +0100

    external DS properties sample

--- di-demo/src/main/java/com/angello/DiDemoApplication.java
+++ di-demo/src/main/java/com/angello/DiDemoApplication.java
@@ -4,6 +4,7 @@ import com.angello.controllers.ConstructorInjectedController;
 import com.angello.controllers.GetterInjectedController;
 import com.angello.controllers.MyController;
 import com.angello.controllers.PropertyInjectedController;
+import com.angello.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,9 +17,15 @@ public class DiDemoApplication {
 
         MyController controller = (MyController) ctx.getBean("myController");
 
-        System.out.println(controller.hello());
-        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+//        System.out.println(controller.hello());
+//        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+//        System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
+//        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+        System.out.println(fakeDataSource.getUser());
+        System.out.println(fakeDataSource.getPassword());
+        System.out.println(fakeDataSource.getUrl());
 	}
 }
