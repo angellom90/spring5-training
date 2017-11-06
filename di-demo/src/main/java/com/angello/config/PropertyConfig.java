b2bb68d54ad9fdeb18907d94aee3ff1f7ac22940
Angello Manrique <angellom90@gmail.com>
2017-11-06 16:46:06 +0100

    enviroment variables spring config

--- di-demo/src/main/java/com/angello/config/PropertyConfig.java
+++ di-demo/src/main/java/com/angello/config/PropertyConfig.java
@@ -1,11 +1,13 @@
 package com.angello.config;
 
 import com.angello.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 /**
  * Created by jt on 6/7/17.
@@ -14,19 +16,19 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
 
+    @Autowired
+    Environment env;
     @Value("${guru.username}")
     String user;
-
     @Value("${guru.password}")
     String password;
-
     @Value("${guru.dburl}")
     String url;
 
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
