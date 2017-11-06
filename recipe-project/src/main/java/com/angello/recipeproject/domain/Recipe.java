9b8268f759c54b32f8bea11d1d7676847c0290e9
Angello Manrique <angellom90@gmail.com>
2017-11-06 22:04:00 +0100

    JPA Enumerations

--- recipe-project/src/main/java/com/angello/recipeproject/domain/Recipe.java
+++ recipe-project/src/main/java/com/angello/recipeproject/domain/Recipe.java
@@ -20,14 +20,15 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -118,5 +119,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
