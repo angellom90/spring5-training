dc11d61486ce0ef08487b926dc06de6b84d365fc
Angello Manrique <angellom90@gmail.com>
2017-11-07 16:04:25 +0100

    fix set test in the controllerTest

--- recipe-project/src/test/java/com/angello/recipeproject/controllers/IndexControllerTest.java
+++ recipe-project/src/test/java/com/angello/recipeproject/controllers/IndexControllerTest.java
@@ -3,12 +3,16 @@ package com.angello.recipeproject.controllers;
 import com.angello.recipeproject.domain.Recipe;
 import com.angello.recipeproject.services.RecipeService;
 import com.angello.recipeproject.services.RecipeServiceImpl;
+import com.sun.org.apache.regexp.internal.RE;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatcher;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.*;
@@ -35,12 +39,27 @@ public class IndexControllerTest {
     @Test
     public void getIndexPage() throws Exception {
 
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setDescription("recipe1");
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
         //when(model.addAttribute("recipes",recipeService.getRecipes())).thenReturn("index");
 
+        //when
         String viewName = indexController.getIndexPage(model);
+
+        //then
         assertEquals("index",viewName);
         verify(recipeService,times(1)).getRecipes();
-        verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+        verify(model,times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2,setInController.size());
     }
 
 }
